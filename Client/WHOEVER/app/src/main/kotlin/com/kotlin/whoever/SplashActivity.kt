9032a89a70a531c7b8906903d3d158f741798f8b
minkyu <minkyu9403@gmail.com>
2018-09-29 14:23:01 +0900

    Client - Splash activity 구현

---
+++ Client/WHOEVER/app/src/main/kotlin/com/kotlin/whoever/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.kotlin.whoever
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.startActivity
+
+class SplashActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity<MainActivity>()
+    }
+}
