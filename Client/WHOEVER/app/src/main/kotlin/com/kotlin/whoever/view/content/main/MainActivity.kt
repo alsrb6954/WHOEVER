da4ece1a20f029335a3a8a0158f573851b5134b4
minkyu <minkyu9403@gmail.com>
2018-10-16 19:22:47 +0900

    Client - floating button Lottie animation으로 진행중

--- Client/WHOEVER/app/src/main/kotlin/com/kotlin/whoever/view/content/main/MainActivity.kt
+++ Client/WHOEVER/app/src/main/kotlin/com/kotlin/whoever/view/content/main/MainActivity.kt
@@ -2,21 +2,73 @@ package com.kotlin.whoever.view.content.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import com.kotlin.whoever.R
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val fab_open by lazy {
+        AnimationUtils.loadAnimation(applicationContext, R.anim.fab_open)
+    }
+    private val fab_close by lazy {
+        AnimationUtils.loadAnimation(applicationContext, R.anim.fab_close)
+    }
+    private var isFabOpen = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        menu_icon.apply { useHardwareAcceleration(true) }
+        menu_icon.useHardwareAcceleration(true)
         menu_icon.setOnClickListener {
                 menu_icon.apply {
                     speed = -speed
                     setMinAndMaxFrame(0, 50)
                     if (!isAnimating) { playAnimation() }
                 }
+        }
+        fab.useHardwareAcceleration(true)
+        fab.setOnClickListener {
+            fab.apply {
+                speed = -speed
+                setMinAndMaxFrame(0, 5)
+                if (!isAnimating) {
+                    playAnimation()
+                }
+            }
+        }
+        fab1.setOnClickListener(this)
+        fab2.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            fab1 -> toast("fab1")
+            fab2 -> toast("fab2")
+        }
+    }
+    fun anim(){
+        if (isFabOpen) {
+            fab1.apply {
+                startAnimation(fab_close)
+                isClickable = false
+            }
+            fab2.apply {
+                startAnimation(fab_close)
+                isClickable = false
+            }
+            isFabOpen = false
+        } else {
+            fab1.apply {
+                startAnimation(fab_open)
+                isClickable = true
+            }
+            fab2.apply {
+                startAnimation(fab_open)
+                isClickable = true
+            }
+            isFabOpen = true
         }
     }
 }
