a3fb6befeb97c66e87af412233a4d3df7e8a81a7
minkyu <minkyu9403@gmail.com>
2018-10-16 16:59:00 +0900

    Client - main 화면 디자인 진행중

--- Client/WHOEVER/app/src/main/kotlin/com/kotlin/whoever/view/content/main/MainActivity.kt
+++ Client/WHOEVER/app/src/main/kotlin/com/kotlin/whoever/view/content/main/MainActivity.kt
@@ -3,11 +3,20 @@ package com.kotlin.whoever.view.content.main
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.whoever.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        menu_icon.apply { useHardwareAcceleration(true) }
+        menu_icon.setOnClickListener {
+                menu_icon.apply {
+                    speed = -speed
+                    setMinAndMaxFrame(0, 50)
+                    if (!isAnimating) { playAnimation() }
+                }
+        }
     }
 }
