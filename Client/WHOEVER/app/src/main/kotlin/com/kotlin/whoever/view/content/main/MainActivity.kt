3f9e647faaa7cc915994748567d03051d5395c4f
minkyu <minkyu9403@gmail.com>
2018-10-18 15:45:13 +0900

    Client - Main activity layout 분해, 초기 디자인 완성( floating button, navigation bar) 등 Lottie로 액티비있게 구현

--- Client/WHOEVER/app/src/main/kotlin/com/kotlin/whoever/view/content/main/MainActivity.kt
+++ Client/WHOEVER/app/src/main/kotlin/com/kotlin/whoever/view/content/main/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.kotlin.whoever.view.content.main
 
-import android.graphics.Color
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.view.animation.AnimationUtils
 import com.kotlin.whoever.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -17,21 +22,41 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private val fab_close by lazy {
         AnimationUtils.loadAnimation(applicationContext, R.anim.fab_close)
     }
+    private lateinit var toggle: ActionBarDrawerToggle
     private var isFabOpen = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        menu_icon.apply {
-            useHardwareAcceleration(true)
-            speed = -1f
-        }
-        menu_icon.setOnClickListener {
+        toggle = object :ActionBarDrawerToggle(this,drawer_layout,R.string.navigation_drawer_open,R.string.navigation_drawer_close){
+            override fun onDrawerClosed(drawerView: View) {
+                super.onDrawerClosed(drawerView)
                 menu_icon.apply {
                     speed = -speed
                     setMinAndMaxFrame(0, 50)
-                    if (!isAnimating) { playAnimation() }
+                    if (!isAnimating) {
+                        playAnimation()
+                    }
                 }
+            }
+        }
+
+        toggle.syncState()
+        drawer_layout.addDrawerListener(toggle)
+
+        menu_icon.apply {
+            useHardwareAcceleration(true)
+            speed = -4f
+        }
+        menu_icon.setOnClickListener {
+            menu_icon.apply {
+                speed = -speed
+                setMinAndMaxFrame(0, 50)
+                if (!isAnimating) {
+                    playAnimation()
+                }
+            }
+            drawer_layout.openDrawer(GravityCompat.START)
         }
         fab.apply {
             useHardwareAcceleration(true)
@@ -50,14 +75,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         fab1.setOnClickListener(this)
         fab2.setOnClickListener(this)
     }
-
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
     override fun onClick(v: View?) {
-        when(v){
+        when (v) {
             fab1 -> toast("fab1")
             fab2 -> toast("fab2")
         }
     }
-    fun anim(){
+
+    fun anim() {
         if (isFabOpen) {
             fab1.apply {
                 startAnimation(fab_close)
